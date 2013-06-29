0cf4fae58481e195585dbb2daae7cc8d5035e124
oh-sky <y_oppa@hotmail.com>
2013-06-29 17:46:06 +0900

    curlの出力をobでキャプチャするように

--- api.php
+++ api.php
@@ -6,5 +6,10 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 
+ob_start();
 curl_exec($ch);
+$yomitan_response = ob_get_clean();
+ob_end_clean();
 curl_close($ch);
+
+echo $yomitan_response;
